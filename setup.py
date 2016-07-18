17d1cea11e0060cc0b18a05708bd4dac6d67571f
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-07-18 10:41:30 +0100

    Corrects support for GitHub dependencies.

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name="cacheusagesimulator",
     version="0.1.0",
     packages=find_packages(exclude=["tests"]),
-    install_requires=[x for x in open("requirements.txt", "r").read().splitlines()]
+    install_requires=[x for x in open("requirements.txt").read().splitlines() if "://" not in x],
+    dependency_links=[x for x in open("requirements.txt").read().splitlines() if "://" in x]
 )
